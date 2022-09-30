87a5758f98e33fd17b8a81c850b3bbafc99cbe45
Andri Sutanto <me@andrisutanto.com>
2022-09-30 20:57:03 +0700

    Delete Movie
    Delete Movie

--- app/Http/Controllers/Admin/MovieController.php
+++ app/Http/Controllers/Admin/MovieController.php
@@ -125,4 +125,11 @@ class MovieController extends Controller
         return redirect()->route('admin.movie')->with('success', 'Movie Updated');
         
     }
+
+    public function destroy($id)
+    {
+        Movie::find($id)->delete();
+
+        return redirect()->route('admin.movie')->with('success', 'Movie Deleted');
+    }
 }
